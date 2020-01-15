75b870728fe53424c216aea48b4489812f2589c9
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-01-15 13:35:10 +0100

    customer class finished

---
+++ php/model/classes/customer.php
@@ -0,0 +1,50 @@
+<?php
+
+class Customer{
+
+    //PROPERTIES
+    private $id;
+    public $name;
+    public $since;
+    public $revenue;
+
+    //CONSTRUCTOR
+    public function __construct($id, $name, $since, $revenue){
+        $this->id = $id;
+        $this->name = $name;
+        $this->since = $since;
+        $this->revenue = $revenue;
+    }
+
+    //METHODS
+    //SETTERS
+    public function setId(){
+        $this->id = $id;
+    }
+    public function setName(){
+        $this->name = $name;
+    }
+    public function setSince(){
+        $this->since = $since;
+    }
+    public function setRevenue(){
+        $this->revenue = $revenue;
+    }
+
+    //GETTERS
+    public function getId(){
+        return $this->id; 
+    }
+    public function getName(){
+        return $this->name; 
+    }
+    public function getSince(){
+        return $this->since; 
+    }
+    public function getRevenue(){
+        return $this->revenue; 
+    }
+}
+
+
+?>
