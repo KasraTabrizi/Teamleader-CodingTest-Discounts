f8f62c13f8a0e1c8d8ede059c0800347535fb46d
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-01-15 13:38:14 +0100

    product.php finished

--- php/model/classes/product.php
+++ php/model/classes/product.php
@@ -1,3 +1,50 @@
 <?php
 
+class Product{
+
+    //PROPERTIES
+    private $id;
+    public $description;
+    public $category;
+    public $price;
+
+    //CONSTRUCTOR
+    public function __construct($id, $description, $category, $price){
+        $this->id = $id;
+        $this->description = $description;
+        $this->category = $category;
+        $this->price = $price;
+    }
+
+    //METHODS
+    //SETTERS
+    public function setId(){
+        $this->id = $id;
+    }
+    public function setDescription(){
+        $this->description = $description;
+    }
+    public function setCategory(){
+        $this->category = $category;
+    }
+    public function setPrice(){
+        $this->price = $price;
+    }
+
+    //GETTERS
+    public function getId(){
+        return $this->id; 
+    }
+    public function getDescription(){
+        return $this->description; 
+    }
+    public function getCategory(){
+        return $this->category; 
+    }
+    public function getPrice(){
+        return $this->price; 
+    }
+}
+
+
 ?>
