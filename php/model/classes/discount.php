dfc526785626ae14c4d0635cf9435b581ebbbcae
KasraTabrizi <kasra_tabrizi@hotmail.com>
2020-01-16 15:23:39 +0100

    updated the comments in discount.php

--- php/model/classes/discount.php
+++ php/model/classes/discount.php
@@ -20,11 +20,14 @@
         public $normalDiscount = 0.2;
 
         //METHODS
-        //function that gives a discount according to how many of a product in a certain category is ordered (Categorie 1)
+        //function that gives a discount according to how many of a product in a certain category is ordered (Category 1)
         private function normalDiscount($totalQuantity, $order, $cheapest){ 
             $totalPrice = 0;
+            //if the total purchase of a category 1 product is more or equal to 2 then add a discount
+            //else just return the unchanged totalprice
             if($totalQuantity >= $this->productAmount){
                 foreach($order->items as $item){
+                    //check which price unit is the cheapest
                     if($item['unit-price'] === $cheapest){
                         $totalPrice += $item['total'] * (1 - $this->normalDiscount);
                     }
@@ -39,7 +42,7 @@
             return $totalPrice;
         }
 
-        //function that gives X amount of a product for free according to how many of that product was ordered (Categorie 2)
+        //function that gives X amount of a product for free according to how many of that product was ordered (Category 2)
         private function getFree($quantity){
             $quotient = $quantity / $this->buyThreshold; 
             return $quotient * $this->freeAmount;
@@ -60,13 +63,16 @@
             $discountStatus = array("freeAmount" => 0, "totalPriceDiscount" => 0, "discountReason" => ""); 
             $cheapest = $order->items[0]['unit-price'];
             $totalQuantity = 0;
+            
             //loop through each item of the order
             foreach($order->items as $item){
                 //check to which categorie the item belongs
                 if(strpos($item['product-id'], "A") !== false){ //Categorie 1
+                    //check which price unit is the cheapest
                     if($item['unit-price'] < $cheapest){
                         $cheapest = $item['unit-price'];
                     }
+                    //count how many items of category 1 is purchased
                     $totalQuantity += $item['quantity'];
                 } 
                 elseif(strpos($item['product-id'], "B") !== false){ //Categorie 2
@@ -74,7 +80,9 @@
                 }
             }
 
+            //perform a normal discount
             $discountStatus['totalPriceDiscount'] = $this->normalDiscount($totalQuantity, $order, $cheapest);
+
             //check if revenue is greater than the threshold
             $discountStatus['totalPriceDiscount'] = $this->revenueDiscount($customer->revenue, $discountStatus['totalPriceDiscount']);
 
